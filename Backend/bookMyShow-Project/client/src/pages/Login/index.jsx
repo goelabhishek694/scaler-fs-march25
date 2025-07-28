a8b477d8d5edacff94b410ef7cf0be3639b74f17
Abhishek <abhishekgoel@gofynd.com>
2025-07-28 08:56:28 +0530

    login page task

--- Backend/bookMyShow-Project/client/src/pages/Login/index.jsx
+++ Backend/bookMyShow-Project/client/src/pages/Login/index.jsx
@@ -1,8 +1,51 @@
-const Login = () => {
+import React from "react";
+import { Button, Form, Input } from "antd";
+import { Link } from "react-router-dom";
+
+function Login() {
   return (
-    <div>
-      This is my Login page
-    </div>
-  )
+    <main className="App-header">
+      <h1>Login to BookMyShow</h1>
+      <section className="mw-500 text-center px-3">
+        <Form layout="vertical">
+          <Form.Item
+            label="Email"
+            name="email"
+            className="d-block"
+            rules={[
+              { required: true, message: "Email is required" },
+              { type: "email", message: "Please enter a valid email" }
+            ]}
+          >
+            <Input type="email" placeholder="Enter your Email" />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            className="d-block"
+            rules={[{ required: true, message: "Password is required" }]}
+          >
+            <Input type="password" placeholder="Enter your Password" />
+          </Form.Item>
+
+          <Form.Item className="d-block">
+            <Button
+              type="primary"
+              block
+              htmlType="submit"
+              style={{ fontSize: "1rem", fontWeight: "600" }}
+            >
+              Login
+            </Button>
+          </Form.Item>
+        </Form>
+        <p>
+          New User? <Link to="/register">Register Here</Link>
+        </p>
+      </section>
+    </main>
+  );
 }
+
 export default Login;
