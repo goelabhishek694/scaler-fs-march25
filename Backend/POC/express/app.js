0dd549177318345f6624ee1edbc7abc0fd694436
Abhishek <abhishekgoel@gofynd.com>
2025-07-28 08:15:57 +0530

    schema, mode, crud

--- Backend/POC/express/app.js
+++ Backend/POC/express/app.js
@@ -10,6 +10,41 @@ app.use(express.json());
 
 //serve static files like html ,css, js images 
 app.use(express.static("public"));
+const password = "VU53NKcgNjEdrtGf";
+const mongoose = require("mongoose");
+const dbURL = `mongodb+srv://goelabhishek694:${password}@cluster0.qz68wqh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.connect(dbURL)
+.then((connection) =>{
+    console.log("connected to DB");
+})
+.catch(err =>{
+    console.log(err);
+});
+
+//schema -> blue print of your collection . 
+
+const productSchema = new mongoose.Schema({
+    product_name: {
+       type: String,
+       required: true
+    },
+    product_price: {
+        type: String,
+        required: true
+    },
+    category:{
+        type: String,
+        required: true
+    },
+    isInStock: {
+        type: Boolean,
+        default: true,
+    }
+}, {timestamps: true});
+
+//model lets us interact with collection . it helps us apply query to retrieve/post/update/delete documents 
+const ProductModel = mongoose.model("products", productSchema);
 
 const loggerMiddleware = (req, res ,next) => {
     console.log(`${new Date().toISOString()} ${req.method} ${req.url}`);
@@ -30,14 +65,62 @@ app.get("/search/:id/:brand", (req, res) => {
     res.send("hello world")
 })
 
-app.post("/api/products", loggerMiddleware, (req,res)=> {
+app.post("/api/products", loggerMiddleware, async (req,res)=> {
     //data sent by FE to BE is stored in body inside request object
-    console.log(req.body);
-    res.send("Received a POST request");
+    const body = req.body; 
+    const product = await ProductModel.create({
+        product_name: body.product_name,
+        product_price: body.product_price,
+        category: body.category
+    });
+    console.log(product);
+    return res.status(201).json({
+        message: "product created"
+    });
+})
+
+app.get("/api/products", loggerMiddleware, async (req,res)=> {
+    const query = req.query;
+    console.log(query);
+    const products = await ProductModel.find(query);
+    // const products = await ProductModel.find({}).where({product_price: {$gte:"50"}});
+    console.log(products);
+    return res.status(200).json({
+        message: "product retrieved",
+        data: products
+    });
+})
+app.get("/api/products/:id", loggerMiddleware, async (req,res)=> {
+    const id = req.params.id;
+    const product = await ProductModel.findById(id);
+    console.log(product);
+    return res.status(200).json({
+        message: "product retrieved",
+        data: product
+    });
+});
+
+app.put("/api/products/:id", loggerMiddleware, async (req, res) => {
+    const id = req.params.id;
+    const update = req.body;
+    const product = await ProductModel.findByIdAndUpdate(id, update, {returnDocument:"after"});
+    console.log(product);
+    return res.status(200).json({
+        message: "product updated",
+        data: product
+    });
+})
+
+app.delete("/api/products/:id", loggerMiddleware, async (req, res) => {
+     const id = req.params.id;
+     const product = await ProductModel.findOneAndDelete({"_id": id});
+     return res.status(200).json({
+        message: "product deleted",
+        data: product
+    });
 })
 
 
 app.listen(3000, () => {
-    console.log("server started on port 3000");
-    
+    console.log("server started on port 3000");    
 })
