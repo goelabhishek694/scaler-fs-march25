049021b67880a2fd75687189d87758eaee8ba5db
Abhishek <abhishekgoel@gofynd.com>
2025-08-04 08:44:36 +0530

    bulding protected route

--- Backend/bookMyShow-Project/client/src/Components/ProtectedRoute.jsx
+++ Backend/bookMyShow-Project/client/src/Components/ProtectedRoute.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom';
 
 function ProtectedRoute({children}) {
     //write logic to fetch user data
+    const {user} = useSelector(state=> state.users);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    
   return (
     <div>{children}</div>
   )
